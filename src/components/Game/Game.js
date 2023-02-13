97f3788448d4e66e25883eb4acc71e4110e28343
hrobarikt <tomas.hrobarik@gmail.com>
2023-02-14 00:45:39 +0100

    exercise 3

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,17 +3,22 @@ import React from "react";
 import { sample } from "../../utils";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput";
-
+import GuessResults from "../GuessResults";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
+  const [guessResults, setGuessResults] = React.useState(["   ", ""]);
+  function addNewGuess(newGuess) {
+    setGuessResults([...guessResults, newGuess]);
+  }
   return (
     <>
-      Put a game here!
-      <GuessInput></GuessInput>
+      <GuessResults guessResults={guessResults}></GuessResults>
+      <GuessInput addNewGuess={addNewGuess}></GuessInput>
     </>
   );
 }
